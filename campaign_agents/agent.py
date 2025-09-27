8c52469f9e69d16c07ee02e245534f64cfa7789d
luisdelgado085 <luisdelgadoo085@gmail.com>
2025-09-27 19:06:44 -0400

    Applied parallel agents (Google_search_agent and reddit_agent) also structured the root_agent properly

--- campaign_agents/agent.py
+++ campaign_agents/agent.py
@@ -1,5 +1,16 @@
-from google.adk.agents import Agent
+from google.adk.agents import ParallelAgent, SequentialAgent, LoopAgent
+from subagents.pytrends_agent import pytrend_agent
 
-root_agent = Agent(
-    name=""
+trend_data_collector = ParallelAgent(
+    name="trend_data_collector",
+    sub_agents=[pytrend_agent,],
+)
+
+campaign_refiner = LoopAgent(
+    name="campaign_refiner",
+)
+
+root_agent = SequentialAgent(
+    name="Control-Agent",
+    sub_agents=[trend_data_collector, campaign_refiner],
 )
