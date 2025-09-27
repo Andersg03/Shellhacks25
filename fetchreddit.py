2e131d1ca46a3c56696b906faaf1ecf716c51eee
luisdelgado085 <luisdelgadoo085@gmail.com>
2025-09-27 15:38:20 -0400

    h

--- fetchreddit.py
+++ fetchreddit.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()  # loads .env into env
 
 reddit = praw.Reddit(
-    client_id=os.environ["REDDIT_CLIENT_ID"],
-    client_secret=os.environ["REDDIT_CLIENT_SECRET"],
+    client_id=os.environ.get("REDDIT_CLIENT_ID"),
+    client_secret=os.environ.get("REDDIT_CLIENT_SECRET"),
     user_agent=os.environ.get("REDDIT_USER_AGENT", "hackathon_app"),
     username=os.environ.get("REDDIT_USERNAME"),
     password=os.environ.get("REDDIT_PASSWORD")
